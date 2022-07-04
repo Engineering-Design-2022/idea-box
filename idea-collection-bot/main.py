4ff483e1cd83db2069e827b7e497f1a498175c12
kathmandu777 <mahhakatomanato@gmail.com>
2022-07-04 14:49:57 +0900

    wip

---
+++ idea-collection-bot/main.py
@@ -0,0 +1,33 @@
+import requests
+import speech_recognition as sr
+
+
+class IdeaCollectionBot:
+    POST_IDEA_URL = "http://localhost:8000/ideas"
+
+    def __init__(self) -> None:
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+
+    def listen(self):
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source)
+            audio = self.recognizer.listen(source)
+        return audio
+
+    def recognize(self, audio) -> str:
+        try:
+            return self.recognizer.recognize_google(audio, language="ja-JP")
+        except sr.UnknownValueError:
+            return "error"
+        except sr.RequestError:
+            return "error"
+
+    def post_idea(self, title: str, description: str, deadline: str):
+        response = requests.post(
+            self.POST_IDEA_URL, json={"title": title, "description": description, "deadline": deadline}
+        )
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return "error"
