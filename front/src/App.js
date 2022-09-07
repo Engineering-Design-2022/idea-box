f80e1614c73de6520650e921c7277cc5ddc6d6ba
Arhikiyu <31932@toyota.kosen-ac.jp>
2022-09-07 19:30:08 +0900

    import-bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 //import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Article } from "./Article";
 import { Comment } from "./Comment";
 
@@ -7,6 +8,12 @@ import { Comment } from "./Comment";
 function App() {
   return (
     <div>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous"
+      />
       <h1>hello world</h1>
       <Article title={"test"} id={"1"} description={"これはテストです"} deadline={"2020 - 01 - 07T00:00: 00Z"} created_at={"2020-01-01T00:00:00Z"} />
       <h3>コメント</h3>
