25652718c784de3da24b02e15df3be7370d12fed
Arhikiyu <31932@toyota.kosen-ac.jp>
2022-09-10 03:31:01 +0900

    make_Article.css

--- front/src/Article.js
+++ front/src/Article.js
@@ -1,9 +1,23 @@
+import './Article.css'
+
+
 export const Article = Article => {
     return (
         <div>
-            <h1>{Article.id} {Article.title}の詳細ページです</h1>
-            <p>{Article.description}</p>
+            <table className="content">
+                <thead>
+                    <tr>
+                        <th>ID</th><th>タイトル</th><th>説明</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{Article.id}</td><td>{Article.title}</td><td>{Article.description}</td>
+                    </tr>
+                </tbody>
+            </table>
             <p>締切 {Article.deadline}  /  作成日 {Article.created_at}</p>
         </div>
-    )
+
+    );
 }
